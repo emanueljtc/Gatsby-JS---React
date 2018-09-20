cdd3e5c93149981a81fd442007301db373ac3ed9
ema18787 <emanueljtc@gmail.com>
2018-09-20 13:14:16 -0400

    cambios

--- src/components/menu.js
+++ src/components/menu.js
@@ -11,7 +11,7 @@ const Menu = () => (
           justifyContent: 'space-evenly'
       }}>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="about">About</Link></li>
       </ul>
     </div>
 )
